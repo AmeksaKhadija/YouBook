2273fbb7570f6fa273cfddeef2d961a80401d639
khadija <ameksakhadija026@gmail.com>
2024-01-31 15:36:29 +0100

    creation des routes corespond books

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomeController::class, 'home'])->name('home');
+Route::resource('books', BookController::class);
